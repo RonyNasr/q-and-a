3936a0bca95ceb51222557238d34b932f6aea80f
Rony Nasr <nasr.rony@gmail.com>
2016-08-19 10:16:40 -0700

    added questions listing

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   author: DS.attr(),
   title: DS.attr(),
   content: DS.attr(),
-  comments: DS.hasMany('comment', {async: true})
+  answers: DS.hasMany('answer', {async: true})
 });
